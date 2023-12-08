93d6f43145d7d782f6843e6781d5bb08a635a62c
Jesus Chicho Hernández <al221710026@gmail.com>
2023-12-07 18:37:09 -0600

    feat: :sparkles: language
    multilanguage functionality is implemented on the page

--- public/assets/js/data.js
+++ public/assets/js/data.js
@@ -1,39 +1,12 @@
-// Services data
-const servicesData = [
+const lastUpdated = '2023-12-07';
+
+const languages = [
     {
-        icono: "ri-layout-3-line",
-        titulo: "Desarrollador web",
-        descripcion: "Como desarrollador web full stack, ofrezco soluciones creativas y personalizadas para diseñar y desarrollar sitios web de alto rendimiento. Con mi experiencia en HTML, CSS, JavaScript y frameworks como VueJs, React y Angular puedo crear interfaces intuitivas y atractivas que brinden una excelente experiencia de usuario."
+        description: 'ES',
+        value: 'es',
     },
     {
-        icono: "ri-code-box-line",
-        titulo: "Desarrollo de API Rest",
-        descripcion: "Con mi experiencia en desarrollo de API Rest, puedo diseñar y construir interfaces de programación de aplicaciones confiables y eficientes. Utilizando tecnologías como Php, Python, Node.js puedo desarrollar API escalables y seguras que permiten la comunicación fluida entre diferentes sistemas y aplicaciones."
-    },
-    {
-        icono: "ri-smartphone-line",
-        titulo: "Desarrollo de aplicaciones móviles",
-        descripcion: "Como desarrollador full stack, puedo crear aplicaciones móviles intuitivas y funcionales. Utilizando tecnologías como Kotlin, Flutter y frameworks nativos de iOS y Android, puedo diseñar y desarrollar aplicaciones móviles robustas que se ajusten a tus necesidades y brinden una experiencia de usuario excepcional."
-    }
-];
-// Projects data
-const projects = [
-    {
-        imgSrc: "public/assets/images/programing.webp",
-        subtitle: "Web",
-        title: "Clinica Dental",
-        demoLink: "javascript:void(0);"
-    },
-    {
-        imgSrc: "public/assets/images/programing.webp",
-        subtitle: "Web",
-        title: "Recursos Humanos",
-        demoLink: "javascript:void(0);"
-    },
-    {
-        imgSrc: "public/assets/images/programing.webp",
-        subtitle: "Web",
-        title: "Página web",
-        demoLink: "javascript:void(0);"
+        description: 'EN',
+        value: 'en',
     }
 ];
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -160,7 +160,7 @@ downloadCv = async () => {
 
 
 const textElement = document.getElementById('typing-text');
-const spanElement = document.getElementById('typing-span');
+const spanElement = document.getElementById('typingText');
 const text = "Chicho_Hernández";
 
 let index = 0;
@@ -195,69 +195,278 @@ const floatWhatsapp = document.querySelector('.FloatWhatsapp');
 handleAnimationHover(floatPhone);
 handleAnimationHover(floatWhatsapp);
 
-// Get the service container
-const servicesContainer = document.getElementById("services-container");
 
-// Dynamically generate services
-servicesData.forEach(servicio => {
-    const article = document.createElement("article");
-    article.className = "services__card";
 
-    const icono = document.createElement("i");
-    icono.className = servicio.icono + " services__icon";
+const languageSelect = document.getElementById('language');
+const languageImage = document.getElementById('languaje'); // Assuming you have set the correct ID for the image
 
-    const titulo = document.createElement("h2");
-    titulo.className = "services__title";
-    titulo.textContent = servicio.titulo;
-
-    const descripcion = document.createElement("p");
-    descripcion.className = "services__description";
-    descripcion.textContent = servicio.descripcion;
-
-    article.appendChild(icono);
-    article.appendChild(titulo);
-    article.appendChild(descripcion);
-
-    servicesContainer.appendChild(article);
+// Iterate over the languages array and add options to the select
+languages.forEach(language => {
+    const option = document.createElement('option');
+    option.value = language.value;
+    option.textContent = language.description;
+    languageSelect.appendChild(option);
 });
 
-const projectsContainer = document.getElementById('projects-container');
-
-projects.forEach(project => {
-    const article = document.createElement('article');
-    article.className = 'projects__card';
-
-    const img = document.createElement('img');
-    img.setAttribute('data-src', project.imgSrc);
-    img.alt = 'projects-img';
-    img.className = 'projects__img lazyload';
-
-    const modalDiv = document.createElement('div');
-    modalDiv.className = 'projects__modal';
-
-    const subtitle = document.createElement('span');
-    subtitle.className = 'projects__subtitle';
-    subtitle.textContent = project.subtitle;
-
-    const title = document.createElement('h2');
-    title.className = 'projects__title';
-    title.textContent = project.title;
-
-    const demoLink = document.createElement('a');
-    demoLink.href = project.demoLink;
-    demoLink.className = 'projects__button';
-    demoLink.textContent = 'Ver demo ';
-
-    const externalLinkIcon = document.createElement('i');
-    externalLinkIcon.className = 'ri-external-link-line';
-    demoLink.appendChild(externalLinkIcon);
-
-    modalDiv.appendChild(subtitle);
-    modalDiv.appendChild(title);
-    modalDiv.appendChild(demoLink);
-
-    article.appendChild(img);
-    article.appendChild(modalDiv);
-
-    projectsContainer.appendChild(article);
+// Toggle language select visibility on image click
+languageImage.addEventListener('click', () => {
+    languageSelect.style.display = languageSelect.style.display === 'none' ? 'block' : 'none';
 });
+
+// Hide language select initially
+languageSelect.style.display = 'none';
+
+
+lastUpdateDate = (selectedLanguage) => {
+    const lastUpdatedElement = document.getElementById("last-updated");
+
+    // Gets the last modified date
+    const lastModified = new Date(`${lastUpdated}T00:00:00`);
+
+    // format date
+    const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+    const locale = selectedLanguage === "en" ? "en-US" : "es-ES";
+
+    const formattedDate = lastModified.toLocaleDateString(locale, options);
+
+    // Update article content with formatted date
+    lastUpdatedElement.textContent = formattedDate; // Set instead of append
+}
+
+// Function to be executed when you change the language
+updateLanguageContent = async (dataLanguage) => {
+    try {
+
+        const menuItems = dataLanguage.header.menuItems;
+        menuItems.forEach(({ id, text }) => {
+            const menuItem = document.getElementById(id);
+            if (menuItem) {
+                menuItem.textContent = text;
+            }
+        });
+
+        // Actualizar contenido de las etiquetas en la sección Home
+        document.getElementById('helloText').textContent = dataLanguage.sections.homeSection.home.hello;
+        document.getElementById('soyText').textContent = dataLanguage.sections.homeSection.home.soy;
+        document.getElementById('homeTitle').textContent = dataLanguage.sections.homeSection.title;
+        document.getElementById('homeEducation').textContent = dataLanguage.sections.homeSection.education;
+        document.getElementById('homeDescription').textContent = dataLanguage.sections.homeSection.description;
+        document.getElementById('homeButton').textContent = dataLanguage.sections.homeSection.button;
+
+        document.getElementById('developmentText').textContent = dataLanguage.sections.homeSection.developmentText;
+        document.getElementById('innovationText').textContent = dataLanguage.sections.homeSection.innovationText;
+        //
+        document.getElementById('aboutSubtitle').textContent = dataLanguage.sections.aboutSection.subtitle;
+        document.getElementById('aboutTitle').textContent = dataLanguage.sections.aboutSection.title;
+        dataLanguage.sections.aboutSection.description.forEach(({ id, text, url }) => {
+            const element = document.getElementById(id);
+            if (element) {
+                // Limpiamos el contenido existente del elemento
+                element.innerHTML = "";
+
+                if (url) {
+                    // Si es un enlace, creamos un elemento <a> y lo agregamos al elemento principal
+                    const linkElement = document.createElement("a");
+                    linkElement.href = url;
+                    linkElement.target = "_blank";
+                    linkElement.rel = "noopener nofollow noreferrer";
+
+                    // Creamos un contenedor <span> para envolver el texto y el elemento <strong>
+                    const spanElement = document.createElement("span");
+                    spanElement.innerHTML = text;
+
+                    // Creamos el elemento <i> y lo agregamos al enlace
+                    const iconElement = document.createElement("i");
+                    iconElement.className = "ri-external-link-line";
+                    linkElement.appendChild(spanElement);
+                    linkElement.appendChild(iconElement);
+
+                    // Agregamos el enlace al elemento principal
+                    element.appendChild(linkElement);
+                } else {
+                    // Si no es un enlace, simplemente agregamos el texto al elemento principal
+                    element.innerHTML = text;
+                }
+            }
+        });
+        document.getElementById('contactButton').textContent = dataLanguage.sections.aboutSection.contactButton;
+
+        const downloadButton = document.getElementById('downloadButton');
+        if (downloadButton) {
+            downloadButton.textContent = dataLanguage.sections.aboutSection.downloadButton.text;
+        }
+        // Actualizar contenido de la sección "skills"
+        document.getElementById('skillsSubtitle').innerHTML = dataLanguage.sections.skillsSection.subtitle;
+        document.getElementById('skillsTitle').textContent = dataLanguage.sections.skillsSection.title;
+        document.getElementById('skillsDescription').textContent = dataLanguage.sections.skillsSection.description;
+        document.getElementById('skillsButton').textContent = dataLanguage.sections.skillsSection.button;
+
+        // Actualizar grupos de habilidades
+        dataLanguage.sections.skillsSection.groups.forEach((group, index) => {
+            const startNumber = group.start;
+            const groupElement = document.getElementById(`skillsGroup${index + 1}`);
+            if (groupElement) {
+                groupElement.innerHTML = ""; // Limpiamos el contenido existente
+
+                group.items.forEach((item, itemIndex) => {
+                    const listItemElement = document.createElement("li");
+                    listItemElement.className = "skills__item";
+                    listItemElement.textContent = item;
+                    groupElement.appendChild(listItemElement);
+                });
+            }
+        });
+
+        // Actualizar contenido de la sección "services"
+        const servicesContainer = document.getElementById("services-container");
+        const servicesSubtitleText = document.getElementById("servicesSubtitleText");
+        const servicesSubtitleSpan = document.getElementById("servicesSubtitleSpan");
+        const servicesTitleText = document.getElementById("servicesTitleText");
+
+        // Limpiar contenido existente
+        servicesContainer.innerHTML = "";
+
+        // Agregar nuevos servicios
+        dataLanguage.sections.servicesSection.dataServices.forEach(servicio => {
+            const article = document.createElement("article");
+            article.className = "services__card";
+
+            const icono = document.createElement("i");
+            icono.className = servicio.icon + " services__icon";
+
+            const titulo = document.createElement("h2");
+            titulo.className = "services__title";
+            titulo.textContent = servicio.title;
+
+            const descripcion = document.createElement("p");
+            descripcion.className = "services__description";
+            descripcion.textContent = servicio.description;
+
+            article.appendChild(icono);
+            article.appendChild(titulo);
+            article.appendChild(descripcion);
+
+            servicesContainer.appendChild(article);
+        });
+
+        // Actualizar subtítulo y título de la sección "services"
+        servicesSubtitleText.textContent = dataLanguage.sections.servicesSection.subtitle;
+        servicesSubtitleSpan.textContent = dataLanguage.sections.servicesSection.subtitleSpan;
+        servicesTitleText.textContent = dataLanguage.sections.servicesSection.title;
+
+
+        // Actualizar contenido de la sección "projects"
+        const projectsContainer = document.getElementById('projects-container');
+        const projectsSubtitleText = document.getElementById('projectsSubtitleText');
+        const projectsSubtitleSpan = document.getElementById('projectsSubtitleSpan');
+        const projectsTitle = document.getElementById('projectsTitle');
+
+        if (projectsContainer && projectsSubtitleText && projectsSubtitleSpan && projectsTitle) {
+            // Actualizar subtítulo y título
+            projectsSubtitleText.textContent = dataLanguage.sections.projectsSection.subtitle;
+            projectsSubtitleSpan.textContent = dataLanguage.sections.projectsSection.subtitleSpan;
+            projectsTitle.textContent = dataLanguage.sections.projectsSection.title;
+
+            // Limpiar contenido existente
+            projectsContainer.innerHTML = "";
+
+            // Agregar nuevos proyectos
+            dataLanguage.sections.projectsSection.dataProjects.forEach(project => {
+                const article = document.createElement('article');
+                article.className = 'projects__card';
+
+                const img = document.createElement('img');
+                img.setAttribute('data-src', project.imgSrc);
+                img.alt = 'projects-img';
+                img.className = 'projects__img lazyload';
+
+                const modalDiv = document.createElement('div');
+                modalDiv.className = 'projects__modal';
+
+                const subtitle = document.createElement('span');
+                subtitle.className = 'projects__subtitle';
+                subtitle.textContent = project.subtitle;
+
+                const title = document.createElement('h2');
+                title.className = 'projects__title';
+                title.textContent = project.title;
+
+                const demoLink = document.createElement('a');
+                demoLink.href = project.demoLink;
+                demoLink.className = 'projects__button';
+                demoLink.textContent = 'View demo ';
+
+                const externalLinkIcon = document.createElement('i');
+                externalLinkIcon.className = 'ri-external-link-line';
+                demoLink.appendChild(externalLinkIcon);
+
+                modalDiv.appendChild(subtitle);
+                modalDiv.appendChild(title);
+                modalDiv.appendChild(demoLink);
+
+                article.appendChild(img);
+                article.appendChild(modalDiv);
+
+                projectsContainer.appendChild(article);
+            });
+        }
+
+
+        document.getElementById('contactSubtitleText').textContent = dataLanguage.sections.contactSection.subtitle;
+        document.getElementById('contactSubtitleSpan').textContent = dataLanguage.sections.contactSection.subtitleSpan;
+        document.getElementById('contactTitle').textContent = dataLanguage.sections.contactSection.title;
+        document.getElementById('nameLabel').textContent = dataLanguage.sections.contactSection.form.nameLabel;
+        document.getElementById('emailLabel').textContent = dataLanguage.sections.contactSection.form.emailLabel;
+        document.getElementById('messageLabel').textContent = dataLanguage.sections.contactSection.form.messageLabel;
+        document.getElementById('user_name').placeholder = dataLanguage.sections.contactSection.form.namePlaceholder;
+        document.getElementById('user_email').placeholder = dataLanguage.sections.contactSection.form.emailPlaceholder;
+        document.getElementById('user_description').placeholder = dataLanguage.sections.contactSection.form.messagePlaceholder;
+        document.getElementById('submitButton').textContent = dataLanguage.sections.contactSection.submitButton;
+
+
+        // Actualizar contenido de la sección de íconos flotantes
+        const phoneIconText = document.getElementById('phoneIconText');
+        const phoneIconTooltip = document.getElementById('phoneIconTooltip');
+
+        if (phoneIconText && phoneIconTooltip) {
+            phoneIconText.textContent = dataLanguage.sections.floatIconsSection.phone.text;
+            phoneIconTooltip.textContent = dataLanguage.sections.floatIconsSection.phone.tooltip;
+
+        }
+        //
+        const footerData = dataLanguage.footer;
+
+        // Actualizar contenido de la sección de footer
+        document.getElementById('footerEducation').textContent = footerData.footerEducation;
+        document.getElementById('copyright').textContent = footerData.copyright;
+        document.getElementById('lastUpdatedText').textContent = footerData.lastUpdatedText;
+        document.getElementById('emailText').textContent = footerData.emailText;
+
+
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Función para cambiar el idioma al hacer clic
+const onChangeLanguage = async () => {
+    try {
+        const selectedLanguage = languageSelect.value;
+        const requestLanguage = await fetch(`public/assets/js/language/${selectedLanguage}.json`);
+        const dataLanguage = await requestLanguage.json();
+        //console.log("dataLanguage", selectedLanguage);
+        //console.log(JSON.stringify(dataLanguage, null, 2));
+
+        // Inicializar o actualizar el contenido al cambiar el idioma
+        lastUpdateDate(selectedLanguage);
+        updateLanguageContent(dataLanguage);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Llamada inicial para cargar el contenido en el idioma actual
+onChangeLanguage();
+// Escuchar cambios en el selector de idioma
+languageSelect.addEventListener('change', onChangeLanguage);
