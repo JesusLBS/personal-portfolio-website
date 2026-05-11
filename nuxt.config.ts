63df060ba33949b536f76866208eab7706e15e89
Jesus Chicho Hernández <chichohdzjesus@gmail.com>
2026-05-10 20:22:47 -0600

    fix: :bug: correct netlify preset and publish dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
   nitro: {
-    preset: 'netlify',
+    preset: 'netlify_builder',
   },
   app: {
     head: {
