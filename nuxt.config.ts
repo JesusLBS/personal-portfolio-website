7eb3a8f0830cef0411906d8c78df9ee1d51db912
Jesus Chicho Hernández <chichohdzjesus@gmail.com>
2026-05-17 00:22:24 -0600

    feat: :lipstick: add Fancybox gallery for project screenshots
    integrate Fancybox lightbox for image previews in projects section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/main.css',
+    '@fancyapps/ui/dist/fancybox/fancybox.css'
+  ],
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@nuxt/image'],
   nitro: {
